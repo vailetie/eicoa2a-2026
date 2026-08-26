d1f97d5c2bdcbd2841adf33fe0e2f58b75a7c5bd
Vailetie <vailetiemuyeza@gmail.com>
2026-08-26 04:23:39 +0200

    Add calc_power function with tests

--- ohms_law.py
+++ ohms_law.py
@@ -13,3 +13,21 @@ def calc_resistance(voltage, current):
         The function raises a ZeroDivisionError if the current is 0.
     """
     return voltage / current
+
+def calc_power(voltage, resistance):
+    """
+    Calculate power dissipated in a resistor using P = V X I.
+    
+    Args:
+        voltage (float): Voltage across the component in Volts (V).
+        resistance (float): The resistance of the component in ohms (Ω). Must be non-zero.
+    
+    Returns:
+        float: Power in watts (W).
+    
+    Notes:
+        Current is derived from Ohm's Law (I = V / R) before calculating power.
+    """
+    current = voltage / resistance
+    power = voltage * current
+    return power
--- test_ohms.py
+++ test_ohms.py
@@ -1,4 +1,4 @@
-from ohms_law import calc_resistance
+from ohms_law import calc_resistance, calc_power
 result = calc_resistance(24, 2)
 print("Resistance =", result, "ohms")
 print(calc_resistance.__doc__)
@@ -7,3 +7,9 @@ print(calc_resistance.__doc__)
 
 assert calc_resistance(9, 0.03) == 300
 assert calc_resistance(24, 2) == 12
+
+print(calc_power(12, 4))
+print(calc_power.__doc__)
+
+assert calc_power(12, 4) == 36
+assert calc_power(10, 5) == 20
